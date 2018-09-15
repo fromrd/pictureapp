656e3238b666c88e1dcef019447aebcfc1257e03
fromrd <rirurarura@gmail.com>
2018-09-15 07:08:59 +0000

    imageのバリデータを追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -52,6 +52,7 @@ class PicturesController < ApplicationController
   
   def confirm
     @picture = Picture.new(picture_params)
+    render 'new'if @picture.invalid?    
   end
   
   def log_in
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,9 @@
 class Picture < ApplicationRecord
-    validates :content, presence: true
-    validates :content, length: {maximum: 140}
-    belongs_to :user
-    has_many :favorites, dependent: :destroy
-    has_many :favorite_users, through: :favorites, source: :user
-    mount_uploader :image, ImageUploader
+  validates :content, presence: true
+  validates :image, presence: true
+  validates :content, length: {maximum: 140}
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+  mount_uploader :image, ImageUploader
 end
